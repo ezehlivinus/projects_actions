9d112b3cb41e02909db2d203c11c1cb7a4453872
Ezeh Livinus <ezehlivinusezeh@gmail.com>
2020-02-10 12:24:38 +0100

    created the viewsets at the view.py

--- the_rest_api/views.py
+++ the_rest_api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_204_NO_CONTENT
+)
+from rest_framework.response import Response
+from the_rest_api.models import Project, Action
+from the_rest_api.serializers import UserSerializer, ProjectSerializer, ActionSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.viewSet):
+    '''
+    Project ViewSet:
+    API endpoint that allows project to be viewed or edited
+    '''
+
+class ActionViewSet(viewsets.viewSet):
+    '''
+    Action ViewSet:
+    API endpoint that allows project to be viewed or edited
+    '''
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryet = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
