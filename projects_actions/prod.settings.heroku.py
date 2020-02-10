ed8d21423808aaee2f77ce9bc9b4aec9bc72f804
Ezeh Livinus <ezehlivinusezeh@gmail.com>
2020-02-10 22:25:38 +0100

    change prod.heroku db to postgres

--- projects_actions/prod.settings.heroku.py
+++ projects_actions/prod.settings.heroku.py
@@ -2,6 +2,18 @@ from .settings import *
 
 import django_heroku
 
+# Redefine the database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'the_rest_api',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 django_heroku.settings(locals())
